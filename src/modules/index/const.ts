5a65f677a79d2406dba4848698e86a57788af581
CesarMC-Port <morecontrol64@gmail.com>
2026-04-25 19:30:23 -0400

    new details

--- src/modules/index/const.ts
+++ src/modules/index/const.ts
@@ -1,8 +1,8 @@
 export const config = {
   teamPhotoEnd: 460,
-  splineEnd: 1400,
-  photo2End: 2200,
-  hidden: `
+  splineEnd: 2000,
+  photo2End: 2800,
+  hidden: ` 
     opacity: 0;
     transform: scale(0) translateY(20px);
     transition: opacity 1s ease, transform 1s ease;
